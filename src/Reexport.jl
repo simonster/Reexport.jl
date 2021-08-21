fd52f68a5cec2c1b9240c7c511eecaf88ac8fd60
lassepe <lasse.peters@mailbox.org>
2021-08-21 22:44:28 +0200

    Format as requested

--- src/Reexport.jl
+++ src/Reexport.jl
@@ -14,13 +14,13 @@ function reexport(m::Module, ex::Expr)
     end
 
     Meta.isexpr(ex, [:module, :using, :import]) ||
-    Meta.isexpr(ex, :toplevel) && all(e->isa(e, Expr) && e.head == :using, ex.args) ||
-    error("@reexport: syntax error")
+        Meta.isexpr(ex, :toplevel) && all(e -> isa(e, Expr) && e.head == :using, ex.args) ||
+        error("@reexport: syntax error")
 
     if ex.head == :module
         modules = Any[ex.args[2]]
         ex = Expr(:toplevel, ex, :(using .$(ex.args[2])))
-    elseif ex.head == :using && all(e->isa(e, Symbol), ex.args)
+    elseif ex.head == :using && all(e -> isa(e, Symbol), ex.args)
         modules = Any[ex.args[end]]
     elseif ex.head == :using && ex.args[1].head == :(:)
         symbols = [e.args[end] for e in ex.args[1].args[2:end]]
@@ -34,8 +34,8 @@ function reexport(m::Module, ex::Expr)
 
     Expr(:toplevel, ex,
          [:(eval(Expr(:export, filter!(x -> Base.isexported($mod, x),
-                                           names($mod; all=true, imported=true))...)))
-              for mod in modules]...)
+                                       names($mod; all=true, imported=true))...)))
+          for mod in modules]...)
 end
 
 export @reexport
